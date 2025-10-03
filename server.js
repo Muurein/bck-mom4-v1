428ac53cd4e0894fa226419478aec654ebd094b6
Muurein <amanda.l.thim@gmail.com>
2025-10-03 22:16:04 +0200

    added JWT

--- server.js
+++ server.js
@@ -14,7 +14,25 @@ app.use(bodyParser.json());
 //router
 app.use("/api", authRoutes);
 
+//skyddad route
+app.get("/api/protected", authenticateToken, (req, res) => {
+    res.json({ message: "Skyddad route" });
+});
 
+//validera jwt-token
+function authenticateToken(req, res, next) {
+    const header = req.headers["authorization"];
+    const jwtToken = header && header.split(" ")[1];
+
+    if (jwtToken == null) res.status(401).json({ message: "Du har inte tillgång till den här routen - token saknas" });
+
+    jwt.verify(jwtToken, process.env.JWT_SECRET_KEY, (err, username) => {
+        if (err) return res.status(403).json({ mesage: "Fel JWT"});
+
+        req.username = username;
+        next();
+    });
+}
 
 //startar app
 app.listen(port, () => {
